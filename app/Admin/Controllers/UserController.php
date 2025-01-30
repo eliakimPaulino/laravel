d46acc535a87cf79b5179419202595fec1a0542e
eliakim <eliakim.paulino@gmail.com>
2025-01-30 14:53:47 -0300

    feat: user grid at laravel admin panel added

---
+++ app/Admin/Controllers/UserController.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Admin\Controllers;
+
+use App\Models\User;
+use Encore\Admin\Controllers\AdminController;
+use Encore\Admin\Form;
+use Encore\Admin\Grid;
+use Encore\Admin\Show;
+use Illuminate\Support\Str;
+
+class UserController extends AdminController
+{
+    /**
+     * Title for current resource.
+     *
+     * @var string
+     */
+    protected $title = 'User';
+
+    /**
+     * Make a grid builder.
+     *
+     * @return Grid
+     */
+    protected function grid()
+    {
+        $grid = new Grid(new User());
+
+        $grid->column('id', __('Id'));
+        $grid->column('token', __('Token'))->display(function ($token) {
+            return Str::limit($token, 10, '...');});
+        $grid->column('name', __('Name'));
+        $grid->column('email', __('Email'));
+        $grid->column('email_verified_at', __('Email verified at'));
+        $grid->column('avatar', __('Avatar'));
+        $grid->column('type', __('Type'));
+        $grid->column('open_id', __('Open id'));
+        $grid->column('access_token', __('Access token'))->display(function ($token) {
+            return Str::limit($token, 10, '...');});
+        $grid->column('deleted_at', __('Deleted at'))->display(function ($token) {
+            return Str::limit($token, 10, '...');});
+        $grid->column('phone', __('Phone'));
+        $grid->column('remember_token', __('Remember token'));
+        $grid->column('created_at', __('Created at'))->display(function ($token) {
+            return Str::limit($token, 10, '...');});
+        $grid->column('updated_at', __('Updated at'))->display(function ($token) {
+            return Str::limit($token, 10, '...');});
+
+        $grid->disableActions();
+        $grid->disableCreateButton();
+        $grid->disableExport();
+        $grid->disableFilter();
+
+
+        return $grid;
+    }
+
+    /**
+     * Make a show builder.
+     *
+     * @param mixed $id
+     * @return Show
+     */
+    protected function detail($id)
+    {
+        $show = new Show(User::findOrFail($id));
+
+        $show->field('id', __('Id'));
+        $show->field('token', __('Token'));
+        $show->field('name', __('Name'));
+        $show->field('email', __('Email'));
+        $show->field('email_verified_at', __('Email verified at'));
+        $show->field('password', __('Password'));
+        $show->field('avatar', __('Avatar'));
+        $show->field('type', __('Type'));
+        $show->field('open_id', __('Open id'));
+        $show->field('access_token', __('Access token'));
+        $show->field('deleted_at', __('Deleted at'));
+        $show->field('phone', __('Phone'));
+        $show->field('remember_token', __('Remember token'));
+        $show->field('created_at', __('Created at'));
+        $show->field('updated_at', __('Updated at'));
+
+        return $show;
+    }
+
+    /**
+     * Make a form builder.
+     *
+     * @return Form
+     */
+    protected function form()
+    {
+        $form = new Form(new User());
+
+        $form->text('token', __('Token'));
+        $form->text('name', __('Name'));
+        $form->email('email', __('Email'));
+        $form->datetime('email_verified_at', __('Email verified at'))->default(date('Y-m-d H:i:s'));
+        $form->password('password', __('Password'));
+        $form->image('avatar', __('Avatar'));
+        $form->number('type', __('Type'));
+        $form->text('open_id', __('Open id'));
+        $form->text('access_token', __('Access token'));
+        $form->mobile('phone', __('Phone'));
+        $form->text('remember_token', __('Remember token'));
+
+        return $form;
+    }
+}
---
+++ app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,143 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+class UserController extends Controller
+{
+    /**
+     * Create User
+     * @param Request $request
+     * @return User 
+     */
+    public function createUser(Request $request)
+    {
+        try {
+            //Validated
+            $validateUser = Validator::make(
+                $request->all(),
+                [
+                    'avatar' => 'required',
+                    'type' => 'required',
+                    'open_id' => 'required',
+                    'name' => 'required',
+                    'email' => 'required',
+                    'password' => 'required|min:6'
+                ]
+            );
+
+            if ($validateUser->fails()) {
+                return response()->json([
+                    'status' => false,
+                    'message' => 'validation error',
+                    'errors' => $validateUser->errors()
+                ], 401);
+            }
+
+            $validated = $validateUser->validated();
+
+            $map = [];
+            $map['type'] = $validated['type'];
+            $map['open_id'] = $validated['open_id'];
+
+            $user = User::where($map)->first();
+
+
+
+            if (empty($user->id)) {
+
+                $validated['token']  = md5(uniqid(mt_rand(10000, 99999)));
+                $validated['created_at'] = Carbon::now();
+
+                // Insira os dados fornecidos no array $validated na tabela users e retorne o ID do registro recém-criado.
+                $validated['password'] = Hash::make($validated['password']);
+                $userID = User::insertGetId($validated);
+
+                // Selecione todos os registros na tabela users onde a coluna id seja igual a $userID.
+                $userInfo = User::where('id', '=', $userID)->first();
+                // Cria um token único de acesso associado ao usuário em $userInfo e armazena seu valor em texto simples na variável $accessToken.
+
+                $accessToken = $userInfo->createToken(uniqid())->plainTextToken;
+
+                $userInfo->access_token = $accessToken;
+
+                User::where('id', '=', $userID)->update(['access_token' => $accessToken]);
+
+                return response()->json([
+                    'code' => 200,
+                    'msg' => 'User Created Successfully',
+                    'data' => $userInfo
+                ], 200);
+            }
+
+            // 144. Backend and modify users table part 9 correction:
+            // $accessToken = $user->createToken(uniqid())->plainTextToken;
+            // $user->access_token = $accessToken;
+            // User::where('open_id', '=', $validated['open_id'])->update(['access_token' => $accessToken]);
+
+            return response()->json([
+                'code' => 200,
+                'msg' => 'User Logged In Successfully',
+                'data' => $user
+            ], 200);
+        } catch (\Throwable $th) {
+            return response()->json([
+                'status' => false,
+                'message' => $th->getMessage()
+            ], 500);
+        }
+    }
+
+    /**
+     * Login The User
+     * @param Request $request
+     * @return User
+     */
+    public function loginUser(Request $request)
+    {
+        try {
+            $validateUser = Validator::make(
+                $request->all(),
+                [
+                    'email' => 'required|email',
+                    'password' => 'required'
+                ]
+            );
+
+            if ($validateUser->fails()) {
+                return response()->json([
+                    'status' => false,
+                    'message' => 'validation error',
+                    'errors' => $validateUser->errors()
+                ], 401);
+            }
+
+            if (!Auth::attempt($request->only(['email', 'password']))) {
+                return response()->json([
+                    'status' => false,
+                    'message' => 'Email & Password does not match with our record.',
+                ], 401);
+            }
+
+            $user = User::where('email', $request->email)->first();
+
+            return response()->json([
+                'status' => true,
+                'message' => 'User Logged In Successfully',
+                'token' => $user->createToken("API TOKEN")->plainTextToken
+            ], 200);
+        } catch (\Throwable $th) {
+            return response()->json([
+                'status' => false,
+                'message' => $th->getMessage()
+            ], 500);
+        }
+    }
+}
