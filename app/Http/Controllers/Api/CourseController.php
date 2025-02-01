7917e885d90a44c02afc739caa89dc68e8ef5e17
eliakim <eliakim.paulino@gmail.com>
2025-01-31 23:21:11 -0300

    post data & retrieve

--- app/Http/Controllers/Api/CourseController.php
+++ app/Http/Controllers/Api/CourseController.php
@@ -3,16 +3,18 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Course;
 use Illuminate\Http\Request;
 
 class CourseController extends Controller
 {
     public function courseList()
     {
+        $result = Course::select('name', 'thumbnail', 'lesson_num', 'price', 'id')->get();
         return response()->json([
             'code' => 200,
             'msg' => 'My course list is here',
-            'data' => 'Data is available'
+            'data' => $result,
         ], 200);
     }
 }
